76c85488a061a822eaeb706c65e55a3235bb618a
namnv2901 <namnv.2901@gmail.com>
2018-11-08 01:55:13 +0700

    struts2 demo

---
+++ struts2demo/src/main/java/namnv/actions/LoginAction.java
@@ -0,0 +1,32 @@
+package namnv.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String userName;
+	private String password;
+	public String login() {
+		if (!userName.equals("namnv") || !password.equals("namnv")) {
+			return ERROR;
+		}
+		return SUCCESS;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
---
+++ struts2demo/src/main/webapp/WEB-INF/pages/welcome.jsp
@@ -0,0 +1,6 @@
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<html>
+<body>
+<h2>Xin chao <s:property value = "userName" /></h2>
+</body>
+</html>
