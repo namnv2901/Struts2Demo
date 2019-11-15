d0dce9980551e02002e3addb51a3e0e4f8e2e1ad
nguyễn nam <namnv.2901@gmail.com>
2019-11-16 00:25:48 +0700

    add action

---
+++ struts2demo/src/main/java/namnv/actions/WelcomeActionCopy.java
@@ -0,0 +1,15 @@
+package namnv.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class WelcomeAction extends ActionSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String initial(){
+		return SUCCESS;
+	}
+}
